6a1d8ce849577440647a18d6f3fcf440c0c8784e
Issa Rice <riceissa@gmail.com>
2018-04-05 12:58:00 -0700

    add field name in fields tuple

--- edit_sql.py
+++ edit_sql.py
@@ -17,9 +17,12 @@ def main():
             m = re.match(r"[^:]*'(https?:\/\/www\.openphilanthropy\.org[^']+)'", line)
             if m:
                 for ending in ["),", ");"]:
-                    if line.strip().endswith(ending):
-                        print(line.strip()[:-2] + ",'" + d[m.group(1)] + "'" +
+                    if line.rstrip().endswith(ending):
+                        print(line.rstrip()[:-2] + ",'" + d[m.group(1)] + "'" +
                               ending)
+            elif line.rstrip().endswith(") values"):
+                l = line.rstrip()
+                print(l[:-len(") values")] + ", grant_stage) values")
             else:
                 print(line, end="")
 
